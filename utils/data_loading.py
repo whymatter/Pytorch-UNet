2406ec0631b849a07b12aee80b2c93120618f269
Oliver <seitz-oliver@gmx.de>
2023-03-12 19:06:05 +0100

    limit scan

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -49,6 +49,9 @@ class BasicDataset(Dataset):
 
         logging.info(f'Creating dataset with {len(self.ids)} examples')
         logging.info('Scanning mask files to determine unique values')
+
+        ids = list(ids[:10])
+
         with Pool() as p:
             unique = list(tqdm(
                 p.imap(partial(unique_mask_values, mask_dir=self.mask_dir, mask_suffix=self.mask_suffix), self.ids),
