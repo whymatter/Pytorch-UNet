acd72da961fdd844210d6a5b62c728e8a96047da
Oliver <seitz-oliver@gmx.de>
2023-06-07 01:30:49 +0200

    revert float 16

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import logging
 import os
 import random
 import sys
-import timeit
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,7 +16,7 @@ from tqdm import tqdm
 import wandb
 from evaluate import evaluate
 from unet import UNet
-from utils.data_loading import BasicDataset, CarvanaDataset, CachedDatset
+from utils.data_loading import BasicDataset
 from utils.dice_score import dice_loss
 
 dir_img = Path('./data/imgs/')
@@ -93,11 +92,8 @@ def train_model(
                     f'but loaded images have {images.shape[1]} channels. Please check that ' \
                     'the images are loaded correctly.'
 
-                start_time = timeit.default_timer()
                 images = images.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
                 true_masks = true_masks.to(device=device, dtype=torch.uint8)
-                elapsed = timeit.default_timer() - start_time
-                # print(f'! Loading took {elapsed}s')
 
                 with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
                     masks_pred = model(images)
