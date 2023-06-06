7da87c7677392646fa3e22a62c2daad65db5144e
Oliver <seitz-oliver@gmx.de>
2023-06-06 14:00:43 +0200

    .

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -128,12 +128,10 @@ class BasicDataset(Dataset):
         logging.info(f'Creating dataset with {len(self.ids)} examples')
         logging.info('Scanning mask files to determine unique values')
 
-        ids = list(self.ids[:10])
-
         with Pool() as p:
             unique = list(tqdm(
                 p.imap(partial(unique_mask_values, mask_dir=self.mask_dir, mask_suffix=self.mask_suffix), ids),
-                total=len(ids)
+                total=len(self.ids)
             ))
 
         self.mask_values = list(sorted(np.unique(np.concatenate(unique), axis=0).tolist()))
